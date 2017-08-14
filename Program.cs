3e62805aeca311674b4d4c9cbe09fd61c0bb01c2
Robby Bourne <robbybourne1@gmail.com>
2017-08-14 14:40:32 -0400

    Input Numbers Working

--- Program.cs
+++ Program.cs
@@ -7,6 +7,9 @@ namespace mathOperations_dotnet
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            double num1 = double.Parse(Console.ReadLine());
+            double num2 = double.Parse(Console.ReadLine());
+            Console.WriteLine(num1 + num2);
         }
-    }
+    } 
 }
