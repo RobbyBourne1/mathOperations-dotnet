5505353de392f8189f713ca750dfdeb01cf74c01
Robby Bourne <robbybourne1@gmail.com>
2017-08-14 15:02:51 -0400

    Refactored The Code

--- Program.cs
+++ Program.cs
@@ -9,11 +9,18 @@ namespace mathOperations_dotnet
             Console.WriteLine("Hello World!");
             double operand1 = double.Parse(Console.ReadLine());
             double operand2 = double.Parse(Console.ReadLine());
-            Console.WriteLine($"The sum of {operand1} and {operand2} is {operand1 + operand2}");
-            Console.WriteLine($"The difference of {operand1} and {operand2} is {operand1 - operand2}");
-            Console.WriteLine($"The product of {operand1} and {operand2} is {operand1 * operand2}");
-            Console.WriteLine($"The quotient of {operand1} and {operand2} is {operand1 / operand2}");
-            Console.WriteLine($"The remainder of {operand1} and {operand2} is {operand1 % operand2}");
+            
+            var sum = operand1 + operand2;
+            var difference = operand1 - operand2;
+            var product = operand1 * operand2;
+            var quotient = operand1 / operand2;
+            var remainder = operand1 % operand2;
+
+            Console.WriteLine($"The sum of {operand1} and {operand2} is {sum}");
+            Console.WriteLine($"The difference of {operand1} and {operand2} is {difference}");
+            Console.WriteLine($"The product of {operand1} and {operand2} is {product}");
+            Console.WriteLine($"The quotient of {operand1} and {operand2} is {quotient}");
+            Console.WriteLine($"The remainder of {operand1} and {operand2} is {remainder}");
         }
     } 
 }
